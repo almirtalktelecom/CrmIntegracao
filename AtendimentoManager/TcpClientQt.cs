ffb526abe3bd3cb8764fda1184209daae818d265
Almir Martinelli <almir.martinelli>
2019-05-15 11:32:20 -0300

    Initial version

---
+++ AtendimentoManager/TcpClientQt.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AtendimentoManager
+{
+    public class TcpClientQt : IDisposable
+    {
+        private readonly IntPtr pClassName;
+
+        [DllImport(@"NovaLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        static private extern IntPtr CreateTcpClient();
+
+        [DllImport(@"NovaLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        static private extern void Open(IntPtr pAtendimentoObject,string servidor, int porta);
+
+        [DllImport(@"NovaLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        static private extern void Send(IntPtr pAtendimentoObject, int len, string p);
+
+        [DllImport(@"NovaLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        static private extern IntPtr ReadData(IntPtr pAtendimentoObject);
+
+        public TcpClientQt()
+        {
+            pClassName = CreateTcpClient();
+        }
+
+        public void Open(string servidor, int porta)
+        {
+            Open(pClassName, servidor, porta);
+        }
+
+        public void Send(int len, string p)
+        {
+            Send(pClassName, len, p);
+        }
+
+        public void ReadData()
+        {
+            ReadData(pClassName);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using AtendimentoManager;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new TcpClientQt();
+
+            // connet to server
+            obj.Open("172.16.5.239", 44900);
+
+            Console.WriteLine("Esc para quit!");
+            for (; ; )
+            {
+                var c = Console.ReadKey();
+                switch (c.Key)
+                {
+                    case ConsoleKey.Escape:
+                        System.Environment.Exit(0);
+                        break;
+
+                    // Send data to server!
+                    case ConsoleKey.Insert:
+                        obj.ReadData();
+                        break;
+
+                    default:
+                        if (c.Key >= ConsoleKey.A && c.Key <= ConsoleKey.Z)
+                        {
+                            var s = c.KeyChar.ToString() + Console.ReadLine();
+                            obj.Send(s.Length, s);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
